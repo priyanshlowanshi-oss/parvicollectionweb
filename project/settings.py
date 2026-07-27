1772a70e6f2a8b0f914e4a02fbf3bb50f4dda83b
priyansh lowanshi <priyanshlowanshi@gmail.com>
2026-07-27 11:56:49 +0530

    super admin

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ CLOUDINARY_URL = config("CLOUDINARY_URL")
 # ==========================
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
@@ -142,6 +142,20 @@ EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# EMAIL_HOST = 'smtp-relay.brevo.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  
+# # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER') 
+# EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+# DEFAULT_FROM_EMAIL = "parvicollection1@gmail.com"
+
 
 # ==========================
 # Default Auto Field
